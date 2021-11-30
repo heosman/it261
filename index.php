85e3d868d9cfdaf858e31e073416accd6cf3bd07
heosman <77591083+heosman@users.noreply.github.com>
2021-11-30 01:43:18 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta name="robots" content="noindex, nofollow">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href="css/styles.css" type=text/css rel="stylesheet">
+<title>Hanan's Portal Page</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="header-inner">
+        <a href="index.php"></a>
+        <img id="logo" src="images/logo.png" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="website/daily.php">Switch (3)</a></li>
+                <li><a href="adder.php">Troubleshooting (4)</a></li>
+                <li><a href="calculator.php">Calculator (5)</a></li>
+                <li><a href="website/contact.php">Email (6)</a></li>
+                <li><a href="website/gallery.php">Gallery (7)</a></li>
+                <li><a href="website/cats.php">Database (8)</a></li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <div id="wrapper">
+    <div id="hero">
+    </div>
+    <main>
+        <h1>Welcome to my portal page!</h1><br><br>
+        <p>Hello, my name is Hanan. I'm currently a sophomore student at Seattle Central College. 
+           I'm in the Web Develepment program. My hobbies include reading, painting, and watching 
+           movies TV shows. My goal is to learn all I can thoroughly so I'm prepared for my future career.</p><br><br>
+        <h2>HW 1 - MAMP Screenshot</h2><br>
+            <img class="screenshots" src="images/mamp-ss.JPG" alt="A screenshot of MAMP localhost"><br><br>
+        <h2>Error Screenshot</h2><br>
+        <img class="screenshots" src="images/error-ss.JPG" alt="A screenshot of the PHP error"><br><br>
+    </main>
+    <aside>
+        <img id="me" src="images/me.png" alt="A photo of me"><br>
+        <h2>My Weekly Class Exercises</h2><br>
+        <h3>Week 2</h3>
+            <ul>
+                <li><a href="weeks/week2/var.php">var.php</a></li>
+                <li><a href="weeks/week2/var2.php">var2.php</a></li>
+                <li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+                <li><a href="weeks/week2/currency.php">currency.php</a></li>
+                <li><a href="weeks/week2/heredoc.php">heredoc.php</a></li>
+            </ul>
+        <h3>Week 3</h3>
+            <ul>
+                <li><a href="weeks/week3/if.php">if.php</a></li>
+                <li><a href="weeks/week3/date.php">date.php</a></li>
+                <li><a href="weeks/week3/forloop.php">forloop.php</a></li>
+                <li><a href="weeks/week3/foreach.php">foreach.php</a></li>
+                <li><a href="weeks/week3/switch.php">switch.php</a></li>
+            </ul>
+        <h3>Week 4</h3>
+            <ul>
+                <li><a href="weeks/week4/form1.php">form1.php</a></li>
+                <li><a href="weeks/week4/form2.php">form2.php</a></li>
+                <li><a href="weeks/week4/form3.php">form3.php</a></li>
+                <li><a href="weeks/week4/form-arithmetic.php">form-arithmetic.php</a></li>
+                <li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+            </ul>
+        <h3>Week 5</h3>
+            <ul>
+                <li><a href="weeks/week5/currency.php">currency.php</a></li>
+                <li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+                <li><a href="weeks/week5/null.php">null.php</a></li>
+                <li><a href="weeks/week5/currency3.php">currency3.php</a></li>
+            </ul>
+        <h3>Week 6</h3>
+            <ul>
+                <li><a href="weeks/week6/form1.php">form1.php</a></li>
+                <li><a href="weeks/week6/functions.php">functions.php</a></li>
+                <li><a href="weeks/week6/implode.php">implode.php</a></li>
+            </ul>   
+        <h3>Week 7</h3>
+            <ul>
+                <li><a href="weeks/week7/random.php">random.php</a></li>
+                <li><a href="weeks/week7/strings.php">strings.php</a></li>
+                <li><a href="weeks/week7/pictures.php">pictures.php</a></li>
+            </ul> 
+        <h3>Week 8</h3>
+            <ul>
+                <li><a href="weeks/week8/people.php">people.php</a></li>
+            </ul> 
+    </aside>
+
+    <footer>
+        <ul>
+            <li>Copyright &copy; 2021</li>
+            <li>All Rights Reserved</li>
+            <li><a href="">Terms of use</a></li>
+            <li><a href="https://validator.w3.org/check/referer" target="_blank">HTML Validation</a></li>
+            <li><a href="https://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">CSS Validation</a></li>
+        </ul>
+    </footer>
+
+    </div>
+</body>
+</html>
