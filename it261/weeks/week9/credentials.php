67d41056ab5d1643b91753b66a0202dcb1634472
heosman <77591083+heosman@users.noreply.github.com>
2021-12-05 23:59:00 -0800

    Add files via upload

---
+++ it261/weeks/week9/credentials.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_NAME', 'hananeoc_fall2021');
+define('DB_USER', 'hananeoc_f21user');
+define('DB_PASSWORD', 'hanan101');
+define('DB_HOST', 'localhost');
---
+++ it261/weeks/week9/index.php
@@ -0,0 +1,49 @@
+<?php 
+
+session_start();
+
+include('config.php');
+
+if(!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = 'You must login first!';
+    header('Location:login.php');
+}
+
+if(isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location:login.php');
+}
+
+include('includes/header.php');
+
+if(isset($_SESSION['success'])) :?>
+<div class="success">
+<h3>
+<?php echo $_SESSION['success']; 
+unset($_SESSION['success']);
+?>
+</h3>
+</div>
+<?php endif; 
+
+if(isset($_SESSION['username'])) :?>
+<div class="welcome-logout">
+<h3>Hello
+<?php echo $_SESSION['username']; ?>
+!</h3>
+<p><a href="index.php?logout='1' ">Log out</a></p>
+</div>
+<?php endif; ?>
+</header>
+
+<div id="wrapper">
+<h1>Welcome to the home page!</h1><br>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. 
+Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. 
+</p>
+
+
+</div>
+<?php include('includes/footer.php');
+
